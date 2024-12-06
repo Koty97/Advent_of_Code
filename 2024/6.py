a5859182e859054d2586c7059a662dc14a69c455
Koty97 <Koty97@email.cz>
2024-12-06 22:17:29 +0100

    Added 2024, day 6, part 2

--- 2024/6.py
+++ 2024/6.py
@@ -8,12 +8,31 @@ def add_direction(direction):
     else:
         return 0
 
-def print_matrix(matrix):
-    for row in matrix:
-        line=""
-        for col in row:
-            line=line+col
-        print(line)
+def simulate_movement(matrix,pos,direction):
+    visited_fields = []
+    player_pos=pos.copy()
+    while (direction == 0 and player_pos[0] != 0) or (direction == 90 and player_pos[1] != len(matrix[0])-1) or (direction == 180 and player_pos[0] != len(matrix)-1) or (direction == 270 and player_pos[1] != 0):
+        if direction == 0 and matrix[player_pos[0] - 1][player_pos[1]]!="#":
+            #print("\tMoving player from [{},{}] to [{},{}]".format(player_pos[0],player_pos[1],player_pos[0]-1,player_pos[1]))
+            player_pos[0] = player_pos[0] - 1
+        elif direction == 90 and matrix[player_pos[0]][player_pos[1]+1]!="#":
+            #print("\tMoving player from [{},{}] to [{},{}]".format(player_pos[0], player_pos[1], player_pos[0],
+            #                                                       player_pos[1]+1))
+            player_pos[1] = player_pos[1] + 1
+        elif direction == 180 and matrix[player_pos[0] + 1][player_pos[1]]!="#":
+            #print("\tMoving player from [{},{}] to [{},{}]".format(player_pos[0], player_pos[1], player_pos[0] + 1,
+            #                                                       player_pos[1]))
+            player_pos[0] = player_pos[0] + 1
+        elif direction == 270 and matrix[player_pos[0]][player_pos[1]-1]!="#":
+            #print("\tMoving player from [{},{}] to [{},{}]".format(player_pos[0], player_pos[1], player_pos[0],
+            #                                                       player_pos[1]-1))
+            player_pos[1] = player_pos[1] - 1
+        else:
+            direction=add_direction(direction)
+        visited_fields.append((player_pos[0], player_pos[1]))
+        if visited_fields.count((player_pos[0], player_pos[1]))==7:
+            return True
+            break
 def first(verbose=False):
     file = open("6.input", "r")
     matrix = []
@@ -27,16 +46,12 @@ def first(verbose=False):
     visited_fields.append((player_pos[0], player_pos[1]))
     while (direction == 0 and player_pos[0] != 0) or (direction == 90 and player_pos[1] != len(matrix[0])-1) or (direction == 180 and player_pos[0] != len(matrix)-1) or (direction == 270 and player_pos[1] != 0):
         if direction == 0 and matrix[player_pos[0] - 1][player_pos[1]]!="#":
-            if verbose: print("Player is moving from [{},{}] to [{},{}]".format(player_pos[0],player_pos[1],player_pos[0]-1,player_pos[1]))
             player_pos[0] = player_pos[0] - 1
         elif direction == 90 and matrix[player_pos[0]][player_pos[1]+1]!="#":
-            if verbose: print("Player is moving from [{},{}] to [{},{}]".format(player_pos[0],player_pos[1],player_pos[0]-1,player_pos[1]))
             player_pos[1] = player_pos[1] + 1
         elif direction == 180 and matrix[player_pos[0] + 1][player_pos[1]]!="#":
-            if verbose: print("Player is moving from [{},{}] to [{},{}]".format(player_pos[0],player_pos[1],player_pos[0]-1,player_pos[1]))
             player_pos[0] = player_pos[0] + 1
         elif direction == 270 and matrix[player_pos[0]][player_pos[1]-1]!="#":
-            if verbose: print("Player is moving from [{},{}] to [{},{}]".format(player_pos[0],player_pos[1],player_pos[0]-1,player_pos[1]))
             player_pos[1] = player_pos[1] - 1
         else:
             if verbose: print("Player would encounter obstacle at [{},{}], changing direction from {} to {}".format(player_pos[0]-1,player_pos[1],direction,add_direction(direction)))
@@ -48,7 +63,21 @@ def first(verbose=False):
 
 def second(verbose=False):
     file = open("6.input", "r")
-    summary = 0
+    matrix = []
+    summary=0
+    direction = 0
+    player_pos = []
+    for line in file:
+        matrix.append(list(line.strip()))
+        if "^" in line:
+            player_pos = [len(matrix) - 1, line.index("^")]
+    for i,row in enumerate(matrix):
+        for j,field in enumerate(row):
+            if field == ".":
+                print("Changing [{},{}] from {} to {}".format(i,j,"#","."))
+                row[j]="#"
+                if simulate_movement(matrix,player_pos,direction):summary=summary+1
+                row[j] = "."
     return summary
 
 
