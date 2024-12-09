3668b989375d9413fc966e39358ac7e3d5f7662f
Koty97 <Koty97@email.cz>
2024-12-09 21:29:52 +0100

    Added 2024, day 8, part 1

--- 2024/8.py
+++ 2024/8.py
@@ -1,21 +1,85 @@
 import os.path
+from itertools import permutations, combinations
 from time import perf_counter
 
 current_day = os.path.basename(__file__.split(".")[0])
+
+
+def get_nodes_distance(loc_a, loc_b):
+    return [loc_b[0] - loc_a[0], loc_b[1] - loc_a[1]]
+
+
 def first(verbose=False):
-    file = open("6.input", "r")
+    file = open("8.input", "r")
     matrix = []
-    visited_fields = []
-    direction = 0
-    summary=0
-    return summary
+    antennas = {}
+    antinodes=[]
+    i = 0
+    for line in file:
+        line_matrix = []
+        j = 0
+        for char in line.strip():
+            if char != ".":
+                if antennas.get(char) is not None:
+                    antennas.get(char).append([i, j])
+                else:
+                    antennas[char] = [[i, j]]
+            line_matrix.append(char)
+            j = j + 1
+        matrix.append(line_matrix)
+        i = i + 1
+    if verbose: print(antennas)
+    max_size=49
+    for key,value in antennas.items():
+        perm = combinations(value,2)
+        for i in perm:
+            distance=get_nodes_distance(i[0],i[1])
+            if 0<=i[0][0]-distance[0]<=max_size and 0<=i[0][1]-distance[1]<=max_size and antinodes.count([i[0][0]-distance[0],i[0][1]-distance[1]])==0:
+                antinodes.append([i[0][0]-distance[0],i[0][1]-distance[1]])
+                if verbose: print("Apeending antinode {} for {}".format([i[0][0]-distance[0],i[0][1]-distance[1]], key))
+            if 0<=i[1][0]+distance[0] <= max_size and 0<=i[1][1]+distance[1] <= max_size and antinodes.count([i[1][0]+distance[0],i[1][1]+distance[1]])==0:
+                antinodes.append([i[1][0]+distance[0],i[1][1]+distance[1]])
+                if verbose: print("Apeending antinode {} for {}".format([i[1][0]+distance[0],i[1][1]+distance[1]],key))
+    if verbose: print(antinodes)
+    return len(antinodes)
 
 
 def second(verbose=False):
-    file = open("6.input", "r")
+    file = open("8.input", "r")
     matrix = []
-    summary=0
-    return summary
+    antennas = {}
+    antinodes=[]
+    i = 0
+    for line in file:
+        line_matrix = []
+        j = 0
+        for char in line.strip():
+            if char != ".":
+                if antennas.get(char) is not None:
+                    antennas.get(char).append([i, j])
+                else:
+                    antennas[char] = [[i, j]]
+            line_matrix.append(char)
+            j = j + 1
+        matrix.append(line_matrix)
+        i = i + 1
+    print(antennas)
+    max_size=49
+    for key,value in antennas.items():
+        perm = combinations(value,2)
+        for i in perm:
+            distance=get_nodes_distance(i[0],i[1])
+
+            if 0<=i[0][0]-distance[0]<=max_size and 0<=i[0][1]-distance[1]<=max_size and antinodes.count([i[0][0]-distance[0],i[0][1]-distance[1]])==0:
+                antinodes.append([i[0][0]-distance[0],i[0][1]-distance[1]])
+                i=[i[0][0]-distance[0],i[0][1]-distance[1]]
+                #print("Apeending antinode {} for {}".format([i[0][0]-distance[0],i[0][1]-distance[1]], key))
+            if 0<=i[1][0]+distance[0] <= max_size and 0<=i[1][1]+distance[1] <= max_size and antinodes.count([i[1][0]+distance[0],i[1][1]+distance[1]])==0:
+                antinodes.append([i[1][0]+distance[0],i[1][1]+distance[1]])
+                i=[i[1][0]+distance[0],i[1][1]+distance[1]]
+                #print("Apeending antinode {} for {}".format([i[1][0]+distance[0],i[1][1]+distance[1]],key))
+    print(antinodes)
+    return len(antinodes)
 
 
 time_start = perf_counter()
--- 2024/9.py
+++ 2024/9.py
@@ -3,30 +3,67 @@ from time import perf_counter
 
 current_day = os.path.basename(__file__.split(".")[0])
 
+def get_nearest_free_space(disk):
+    for i,element in enumerate(disk):
+        if "." in element:
+            return i
 
 def first(verbose=False):
     file = open("9.input", "r")
     disk = []
     id=0
     free_space = False
+    free_space_count=0
     while True:
         number = file.read(1)
         if number!="":
             if free_space:
                 if number!=str(0):
                     disk.append(int(number)*".")
+                    free_space_count=free_space_count+int(number)
             else:
-                disk.append(int(number)*str(id))
+                disk.append((int(number)*(str(id)+"-"))[:-1])
                 id=id+1
             free_space = not free_space
         if not number:
             break
     print(disk)
-    for i,element in enumerate(disk):
-        if "." in element:
-            disk[i]="X"
-    print(disk)
+    reverse_disk=disk.copy()
+    reverse_disk.reverse()
+    element_count=0
+    for element in reverse_disk:
+        if element_count==free_space_count:
+            break
+        if "." not in element:
+            if element_count + len(element.split("-"))>free_space_count:
+                print("Should move {} but only {}".format(element,free_space_count-element_count))
+
+                element_count=element_count+free_space_count-element_count
+            else:
+                element_count = element_count + len(element.split("-"))
+                original_length=len(disk[get_nearest_free_space(disk)])
+                if original_length>=len(element.split("-")):
+                    disk[get_nearest_free_space(disk)]=element.replace("-","") + (original_length-len(element.split("-")))*"."
+                    disk[disk.index(element)]=len(element.split("-"))*"."
+                else:
+                    original_element=element
+                    while element!="":
+                        if get_nearest_free_space(disk)==disk.index(original_element):
+                            break
+                        dot_count = disk[get_nearest_free_space(disk)].count(".")
+                        disk[get_nearest_free_space(disk)]=disk[get_nearest_free_space(disk)].replace(".",element.split("-")[0])
+                        element=element[:-2*dot_count]
+                        disk[disk.index(original_element)] = element+(dot_count+original_element.count("."))*"."
+                        original_element=element+(dot_count+original_element.count("."))*"."
+    st=""
+    for element in disk:
+        st=st+element.replace("-","")
     summary = 0
+    for i,char in enumerate(st):
+        if char==".":
+            break
+        summary=summary+(i*int(char))
+    print(disk)
     return summary
 
 
