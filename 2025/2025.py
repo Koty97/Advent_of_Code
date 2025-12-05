164a60af52f731526c2eebabf9c4ad7329c72ba3
Koty97 <Koty97@email.cz>
2025-12-05 17:55:02 +0100

    Added whole year timer

--- 2025/2025.py
+++ 2025/2025.py
@@ -1,9 +1,11 @@
 import os
 import subprocess
 import sys
+from time import perf_counter
 
 current_file = os.path.basename(__file__)
 folder = os.getcwd()
+time_start = perf_counter()
 
 for file in os.listdir(folder):
     if file.endswith(".py") and file != current_file:
@@ -15,3 +17,5 @@ for file in os.listdir(folder):
             text=True
         )
         print(result.stdout)
+time_end = perf_counter()
+print(f'Whole year took {time_end - time_start} seconds')
